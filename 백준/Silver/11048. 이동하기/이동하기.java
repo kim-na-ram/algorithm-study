6e89dd9d98138b1d0c8456917d1cbe478f9831fe
na.ram <32188154+kim-na-ram@users.noreply.github.com>
2025-04-11 17:29:26 +0900

    [Silver II] Title: 이동하기, Time: 480 ms, Memory: 79556 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static int[] dr = {1, 0, 1};
+    public static int[] dc = {0, 1, 1};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] maze = new int[n + 1][m + 1];
+        int[][] dp = new int[n + 1][m + 1];
+
+        for(int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 1; j <= m; j++) {
+                maze[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                dp[i][j] = maze[i][j] + Math.max(dp[i - 1][j], Math.max(dp[i][j - 1], dp[i - 1][j - 1]));
+            }
+        }
+
+        System.out.println(dp[n][m]);
+    }
+}
