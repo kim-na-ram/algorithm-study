b7921df611a5693bc8c4ae8e2ffa9aff4acf6fc5
na.ram <32188154+kim-na-ram@users.noreply.github.com>
2025-04-21 23:32:34 +0900

    [Silver III] Title: N번째 큰 수, Time: 2012 ms, Memory: 340232 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -1,34 +1,23 @@
-import java.util.StringTokenizer;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.Collections;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int n = Integer.parseInt(st.nextToken());
-		
-		int[][] arr = new int[n][n];
-		ArrayList<Integer> list = new ArrayList<Integer>();
-		
-		for(int i = 0; i < n; i++) {
-		    st = new StringTokenizer(br.readLine(), " ");
-		    for(int j = 0; j < n; j++)
-		        list.add(Integer.parseInt(st.nextToken()));
-		}
-		
-		Collections.sort(list, new Comparator<Integer>() {
-		    @Override
-		    public int compare(Integer a, Integer b) {
-		        return b - a;
-		    }
-		});
-		
-		System.out.println(list.get(n-1));
-	}
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        List<Integer> list = new ArrayList<Integer>();
+
+        StringTokenizer st;
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < N; j++) {
+                list.add(Integer.parseInt(st.nextToken()));
+            }
+        }
+
+        Collections.sort(list, (o1, o2) -> o2 - o1);
+
+        System.out.println(list.get(N - 1));
+    }
 }
