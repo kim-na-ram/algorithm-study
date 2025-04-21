c50c6be0a0e4dbf80abe997c234e18ae2ce605e1
na.ram <32188154+kim-na-ram@users.noreply.github.com>
2025-04-21 23:23:42 +0900

    [Silver IV] Title: 큐 2, Time: 1292 ms, Memory: 408104 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.java
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -1,121 +1,39 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
-class Queue {
-    
-    private int front;
-    private int rear;
-    private int max;
-    private int num;
-    private int[] queue;
-    
-    public Queue(int max) {
-        
-        front = rear = num = 0;
-        this.max = max;
-        
-        queue = new int[max];
-        
-    }
-    
-    public void push(int x) {
-        
-        if(num < max) {
-        
-            queue[rear++] = x;
-            num++;
-            
-            if(rear == max) {
-                
-                rear = 0;
-                
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < N; i++) {
+            String[] cmd = br.readLine().split(" ");
+
+            switch(cmd[0]) {
+                case "push":
+                    queue.addLast(Integer.parseInt(cmd[1]));
+                    break;
+                case "pop":
+                    sb.append(queue.isEmpty() ? -1 : queue.removeFirst()).append("\n");
+                    break;
+                case "front":
+                    sb.append(queue.isEmpty() ? -1 : queue.get(0)).append("\n");
+                    break;
+                case "back":
+                    sb.append(queue.isEmpty() ? -1 : queue.get(queue.size() - 1)).append("\n");
+                    break;
+                case "size":
+                    sb.append(queue.size()).append("\n");
+                    break;
+                case "empty":
+                    sb.append(queue.isEmpty() ? 1 : 0).append("\n");
+                    break;
             }
-        
         }
-        
-    }
-    
-    public int pop() {
-        
-        if(num <= 0) {
-            
-            return -1;
-            
-        }
-        
-        int x = queue[front++];
-        num--;
-        
-        if(front == max) {
-            front = 0;
-        }
-        
-        return x;
-        
-    }
-    
-    public int size() {
-        
-        return num;
-        
-    }
-    
-    public int empty() {
-        
-        if(num <= 0) return 1;
-        else return 0;
-        
-    }
-    
-    public int front() {
-        
-        if(num > 0) return queue[front];
-        else return -1;
-        
-    }
-    
-    public int back() {
-        
-        if(num > 0) return queue[rear - 1];
-        else return -1;
-        
-    }
-    
-}
 
-public class Main {
-	public static void main(String[] args) throws IOException {
-	    
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    
-	    int N = Integer.parseInt(br.readLine());
-	    
-	    Queue que = new Queue(N);
-	    
-	    StringBuilder sb = new StringBuilder();
-	    
-	    for(int i = 0; i < N; i++) {
-	        
-	        String command = br.readLine();
-	        
-	        if(command.contains("push")) {
-	            que.push(Integer.parseInt(command.split(" ")[1]));
-	        } else if(command.contains("pop")) {
-	            sb.append(que.pop()).append("\n");
-	        } else if(command.contains("front")) {
-	            sb.append(que.front()).append("\n");
-	        } else if(command.contains("back")) {
-	            sb.append(que.back()).append("\n");
-	        } else if(command.contains("size")) {
-	            sb.append(que.size()).append("\n");
-	        } else if(command.contains("empty")) {
-	            sb.append(que.empty()).append("\n");
-	        }
-	        
-	    }
-	    
-	    System.out.println(sb);
-	    
-	}
+        System.out.println(sb.toString());
+    }
 }
