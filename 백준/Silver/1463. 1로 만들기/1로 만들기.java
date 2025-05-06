785727d575583e4c01b5529185b3eb7530922415
na.ram <32188154+kim-na-ram@users.noreply.github.com>
2025-05-06 17:08:04 +0900

    [Silver III] Title: 1로 만들기, Time: 208 ms, Memory: 21808 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,33 +1,21 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-import java.util.Arrays;
+import java.util.*;
 
 class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int n = Integer.parseInt(br.readLine());
-        
-        int[] dp = new int[n + 1];
-        
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        
-        dp[n] = 0;
-        int index = 0;
-        while(dp[1] == Integer.MAX_VALUE) {
-            for(int i = 1; i < n + 1; i++) {
-                if(dp[i] != index) continue;
-                
-                if(i % 3 == 0) dp[i / 3] = Math.min(index + 1, dp[i / 3]);
-                if(i % 2 == 0) dp[i / 2] = Math.min(index + 1, dp[i / 2]);
-                if(i - 1 >= 1) dp[i - 1] = Math.min(index + 1, dp[i - 1]);
-            }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-            index++;
+        int N = sc.nextInt();
+        int[] dp = new int[N + 1];
+
+        Arrays.fill(dp, Integer.MAX_VALUE);
+
+        dp[N] = 0;
+        for(int i = N; i > 0; i--) {
+            if(i % 3 == 0) dp[i / 3] = Math.min(dp[i / 3], dp[i] + 1);
+            if(i % 2 == 0) dp[i / 2] = Math.min(dp[i / 2], dp[i] + 1);
+            if(i - 1 >= 1) dp[i - 1] = Math.min(dp[i - 1], dp[i] + 1);
         }
-        
+
         System.out.println(dp[1]);
     }
 }
