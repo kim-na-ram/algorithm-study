401966e84017b624025acea72b3a68850cebbfa6
na.ram <32188154+kim-na-ram@users.noreply.github.com>
2025-05-17 21:32:39 +0900

    [Silver II] Title: 유기농 배추, Time: 148 ms, Memory: 16080 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,92 +1,80 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-    
-    public static int N, M, K, count;
-    public static int[][] farm;
-    public static boolean[][] visited;
-    
-    public static void dfs(int i, int j) {
+class Main {
+    public static int N, M;
+    public static int[][] field;
+    public static boolean[][] checked;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         
-        visited[i][j] = true;
+        while(T --> 0) {
+            String[] s = br.readLine().split(" ");
+
+            // 가로길이
+            M = Integer.parseInt(s[0]);
+            // 세로길이
+            N = Integer.parseInt(s[1]);
+            // 심겨진 배추의 개수
+            int K = Integer.parseInt(s[2]);
+
+            // 배추밭
+            field = new int[N][M];
+
+            StringTokenizer st;
+            for(int i = 0; i < K; i++) {
+                st = new StringTokenizer(br.readLine());
+
+                int X = Integer.parseInt(st.nextToken());
+                int Y = Integer.parseInt(st.nextToken());
+
+                // 배추가 심어진 곳
+                field[Y][X] = 1;
+            }
+
+            int count = 0;
+            checked = new boolean[N][M];
+            for(int i = 0; i < N; i++) {
+                for(int j = 0; j < M; j++) {
+                    if(field[i][j] == 1 && !checked[i][j]) {
+                        bfs(i, j);
+                        count++;
+                    }
+                }
+            }
+
+            sb.append(count).append("\n");
         
-        if(checkCabbage(i - 1, j)) {
-            farm[i - 1][j] = count;
-            dfs(i - 1, j);
         }
-            
-        if(checkCabbage(i + 1, j)) {
-            farm[i + 1][j] = count;
-            dfs(i + 1, j);
+
+        System.out.println(sb.deleteCharAt(sb.length() - 1).toString());
+    }
+
+    public static int[] dx = {-1, 0, 1, 0};
+    public static int[] dy = {0, -1, 0, 1};
+    public static void bfs(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+
+        queue.offer(new int[]{x, y});
+        checked[x][y] = true;
+
+        while(!queue.isEmpty()) {
+            int[] pos = queue.poll();
+
+            for(int i = 0; i < dx.length; i++) {
+                int nx = pos[0] + dx[i];
+                int ny = pos[1] + dy[i];
+
+                if(nx < 0 || ny < 0 || nx >= N || ny >= M
+                      || field[nx][ny] == 0 || checked[nx][ny]) continue;
+
+                queue.offer(new int[]{nx, ny});
+                checked[nx][ny] = true;
+            }
         }
-            
-        if(checkCabbage(i, j - 1)) {
-            farm[i][j - 1] = count;
-            dfs(i, j - 1);
-        }
-            
-        if(checkCabbage(i, j + 1)) {
-            farm[i][j + 1] = count;
-            dfs(i, j + 1);
-        }
-            
         
     }
-    
-    public static boolean checkCabbage(int row, int col) {
-        
-        if(row >= N || col >= M || row < 0 || col < 0)
-            return false;
-        if(farm[row][col] != 1 || visited[row][col] == true)
-            return false;
-            
-        return true;
-        
-    }
-    
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int T = Integer.parseInt(br.readLine()); // 테스트케이스
-		
-		StringBuilder sb = new StringBuilder();
-		
-		while(T --> 0) {
-		
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
-		    N = Integer.parseInt(st.nextToken());
-		    M = Integer.parseInt(st.nextToken());
-		    K = Integer.parseInt(st.nextToken());
-		
-		    farm = new int[N][M];
-		    visited = new boolean[N][M];
-		    
-		    count = 0;
-		    
-		    for(int i = 0; i < K; i++) {
-		    	st = new StringTokenizer(br.readLine(), " ");
-		    	int x = Integer.parseInt(st.nextToken());
-		    	int y = Integer.parseInt(st.nextToken());
-		    	
-		    	farm[x][y] = 1;
-		    }
-		
-		    for(int i = 0; i < N; i++)
-		        for(int j = 0; j < M; j++)
-		            if(farm[i][j] == 1 && visited[i][j] == false) {
-        		        count++;
-        		        dfs(i, j);
-		            }
-		            
-		    sb.append(count).append("\n");
-	    }
-	    
-	    System.out.println(sb);
-	
-	}
 }
