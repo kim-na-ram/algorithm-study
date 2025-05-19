743402efef796c49ccf211d114fdb6f96aa44357
na.ram <32188154+kim-na-ram@users.noreply.github.com>
2025-05-19 21:56:26 +0900

    [Silver II] Title: 상자넣기, Time: 120 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Silver/1965. 상자넣기/상자넣기.java
+++ 백준/Silver/1965. 상자넣기/상자넣기.java
@@ -2,45 +2,42 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    public static int[] lis;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int[] boxes = new int[n];
 
+        int[] boxes = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < n; i++) {
             boxes[i] = Integer.parseInt(st.nextToken());
         }
+
+        int[] lcs = new int[n];
+        int lcsLen = 0;
+
+        lcs[lcsLen++] = boxes[0];
         
-        lis = new int[n + 1];
-        int len = 0;
-        lis[len++] = boxes[0];
         for(int i = 1; i < n; i++) {
-            if(lis[len - 1] < boxes[i]) {
-                lis[len++] = boxes[i];
-            } else {
-                int idx = binarySearch(0, len, boxes[i]);
-                lis[idx] = boxes[i];
+            if(lcs[lcsLen - 1] < boxes[i]) lcs[lcsLen++] = boxes[i];
+            else {
+                int low = 0;
+                int high = lcsLen - 1;
+
+                while(low < high) {
+                    int mid = (low + high) / 2;
+
+                    if(lcs[mid] < boxes[i]) {
+                        low = mid + 1;
+                    } else {
+                        high = mid;
+                    }
+                }
+
+                lcs[low] = boxes[i];
             }
         }
-
-        System.out.println(len);
-    }
-
-    public static int binarySearch(int start, int end, int num) {
-        int mid = 0;
-        while(start < end) {
-            mid = (start + end) / 2;
-
-            if(lis[mid] >= num) {
-                end = mid;
-            } else {
-                start = mid + 1;
-            }
-        }
-
-        return start;
+        
+        System.out.println(lcsLen);
     }
 }
