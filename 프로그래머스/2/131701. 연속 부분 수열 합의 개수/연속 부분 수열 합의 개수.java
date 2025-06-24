5178a1d853a7ae89279bf27c03306c91ae293826
na.ram <32188154+kim-na-ram@users.noreply.github.com>
2025-06-24 15:39:24 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 144.99 ms, Memory: 136 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -0,0 +1,18 @@
+import java.util.Set;
+import java.util.HashSet;
+
+class Solution {
+    public int solution(int[] elements) {
+        Set<Integer> set = new HashSet<>();
+        
+        int[] dp = new int[elements.length + 1];
+        for(int i = 1; i < elements.length + 1; i++) {
+            for(int j = 0; j < elements.length; j++) {
+                dp[i] += elements[(i + j - 1) % elements.length];
+                set.add(dp[i]);
+            }
+        }
+        
+        return set.size();
+    }
+}
