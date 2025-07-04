e5ba0230de32d0c8d17e0c9e607291d4e6a396d3
na.ram <32188154+kim-na-ram@users.noreply.github.com>
2025-07-04 22:36:49 +0900

    [level 1] Title: 햄버거 만들기, Time: 71.79 ms, Memory: 131 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+        
+        Stack<Integer> stk = new Stack<>();
+        
+        for(int i : ingredient) {
+            stk.push(i);
+            
+            if(stk.size() >= 4) {
+				int size = stk.size();
+				if(stk.get(size - 1) == 1
+						&& stk.get(size - 2) == 3
+						&& stk.get(size - 3) == 2
+						&& stk.get(size - 4) == 1) {
+					answer++;
+					stk.pop();
+					stk.pop();
+					stk.pop();
+					stk.pop();
+				}
+            }
+        }
+        return answer;
+    }
+}
