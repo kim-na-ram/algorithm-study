c55eb41c5af36fb46d6aa3b76b6202e17b275c17
na.ram <32188154+kim-na-ram@users.noreply.github.com>
2025-08-24 22:51:17 +0900

    [Gold IV] Title: 호텔, Time: 100 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Gold/1106. 호텔/호텔.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int C = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        int[] dp = new int[C + 101];
+        Arrays.fill(dp, 987654321);
+        dp[0] = 0;
+        
+        for(int i = 0; i < N; i++) {
+            st  = new StringTokenizer(br.readLine());
+            int cost = Integer.parseInt(st.nextToken());
+            int customer = Integer.parseInt(st.nextToken());
+            
+            for(int j = customer; j < C + 101; j++) {
+                dp[j] = Math.min(dp[j], cost + dp[j - customer]);
+            }
+        }
+
+        int result = Integer.MAX_VALUE;
+        for(int i = C; i < C + 101; i++) {
+            result = Math.min(result, dp[i]);
+        }
+
+        System.out.print(result);
+    }
+}
