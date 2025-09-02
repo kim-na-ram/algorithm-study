15848d106563c49d5ae43898db1d7920690919e7
na.ram <32188154+kim-na-ram@users.noreply.github.com>
2025-09-02 23:55:36 +0900

    [level 2] Title: 주식가격, Time: 19.79 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,21 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        
+        int[] answer = new int[prices.length];
+        
+        for(int i = 0; i < prices.length; i++) {
+            for(int j = i + 1; j < prices.length; j++) {
+                answer[i] += 1;
+                if(prices[i] > prices[j]) {
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+        
+    }
+}
