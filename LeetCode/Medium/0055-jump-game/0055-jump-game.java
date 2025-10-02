36eeae2724c8492c23cfc5e9cc80e62990656ddf
kim-na-ram <snrneh3@naver.com>
2025-10-02 12:31:16 +0000

    Time: 1 ms (99.92%), Space: 46 MB (31.92%) - LeetHub

--- LeetCode/Medium/0055-jump-game/0055-jump-game.java
+++ LeetCode/Medium/0055-jump-game/0055-jump-game.java
@@ -1,17 +1,11 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
-        dp[0] = 0;
+        int goal = nums.length - 1;
 
-        for(int i = 0; i < nums.length; i++) {
-            if(dp[i] != -1) {
-                for(int j = 1; j <= nums[i]; j++) {
-                    if(i + j < nums.length) dp[i + j] = i;
-                }
-            }
+        for(int i = nums.length - 2; i >= 0; i--) {
+            if(i + nums[i] >= goal) goal = i;
         }
 
-        return dp[nums.length - 1] == -1 ? false : true;
+        return goal == 0;
     }
 }
