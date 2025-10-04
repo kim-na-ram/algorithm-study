5f23effce8c7d3f3a683c3fad11e79026d27f9ee
kim-na-ram <snrneh3@naver.com>
2025-10-04 05:54:33 +0000

    Time: 3 ms (96.25%), Space: 57.7 MB (73.2%) - LeetHub

--- LeetCode/Medium/0011-container-with-most-water/0011-container-with-most-water.java
+++ LeetCode/Medium/0011-container-with-most-water/0011-container-with-most-water.java
@@ -4,12 +4,14 @@ class Solution {
         int end = height.length - 1;
         int max = 0;
 
-        while(start <= end) {
-            int min = Math.min(height[start], height[end]);
-            max = Math.max(max, (min * (end - start)));
-
-            if(height[start] < height[end]) start++;
-            else end--;
+        while(start < end) {
+            if(height[start] < height[end]) {
+                max = Math.max(max, (end - start) * height[start]);
+                start += 1;
+            } else {
+                max = Math.max(max, (end - start) * height[end]);
+                end -= 1;
+            }
         }
 
         return max;
