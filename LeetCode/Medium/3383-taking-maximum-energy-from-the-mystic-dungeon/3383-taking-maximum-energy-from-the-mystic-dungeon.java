323cfa2bdf2e2a5e7a013ae4893ce46eb9b30990
kim-na-ram <snrneh3@naver.com>
2025-10-10 15:31:16 +0000

    Time: 3 ms (60.69%), Space: 61.4 MB (52.41%) - LeetHub

--- LeetCode/Medium/3383-taking-maximum-energy-from-the-mystic-dungeon/3383-taking-maximum-energy-from-the-mystic-dungeon.java
+++ LeetCode/Medium/3383-taking-maximum-energy-from-the-mystic-dungeon/3383-taking-maximum-energy-from-the-mystic-dungeon.java
@@ -5,9 +5,7 @@ class Solution {
         int max = Integer.MIN_VALUE;
 
         for(int i = n - 1; i >= 0; i--) {
-            if(i + k < n) dp[i] = dp[i + k] + energy[i];
-            else dp[i] = energy[i];
-
+            dp[i] = energy[i] + (i + k < n ? dp[i + k] : 0);
             max = Math.max(max, dp[i]);
         }
 
