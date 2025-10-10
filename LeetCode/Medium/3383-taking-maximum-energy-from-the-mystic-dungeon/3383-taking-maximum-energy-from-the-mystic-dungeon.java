8ee8d68eb187548596a4ca408bbdbdcf19d16979
kim-na-ram <snrneh3@naver.com>
2025-10-10 05:09:27 +0000

    Time: 3 ms (60.69%), Space: 63.2 MB (26.9%) - LeetHub

--- LeetCode/Medium/3383-taking-maximum-energy-from-the-mystic-dungeon/3383-taking-maximum-energy-from-the-mystic-dungeon.java
+++ LeetCode/Medium/3383-taking-maximum-energy-from-the-mystic-dungeon/3383-taking-maximum-energy-from-the-mystic-dungeon.java
@@ -3,19 +3,12 @@ class Solution {
         int n = energy.length;
         int[] dp = new int[n];
         int max = Integer.MIN_VALUE;
-        Arrays.fill(dp, max);
 
-        // for(int i = n - 1; i >= 0; i--) {
-        //     dp[i] = Math.max(dp[i], energy[i]);
-        //     if(i - k >= 0) dp[i - k] = Math.max(dp[i - k], dp[i] + energy[i - k]);
+        for(int i = n - 1; i >= 0; i--) {
+            if(i + k < n) dp[i] = dp[i + k] + energy[i];
+            else dp[i] = energy[i];
 
-        //     max = Math.max(dp[i], max);
-        // }
-        
-        for(int i = 0; i < n; i++) {
-            dp[i] = Math.max(dp[i], energy[i]);
-            if(i + k < n) dp[i + k] = Math.max(dp[i + k], dp[i] + energy[i + k]);
-            if(i + k >= n) max = Math.max(dp[i], max);
+            max = Math.max(max, dp[i]);
         }
 
         return max;
