41e8135ca93106e8a0a350a0d79033cea509c987
na.ram <32188154+kim-na-ram@users.noreply.github.com>
2025-10-20 13:07:34 +0900

    Time: 1 ms (74.53%), Space: 42.6 MB (77.82%) - LeetHub

---
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+        for(String operation : operations) {
+            if(operation.contains("+")) {
+                x += 1;
+            } else {
+                x -= 1;
+            }
+        }
+
+        return x;
+    }
+}
