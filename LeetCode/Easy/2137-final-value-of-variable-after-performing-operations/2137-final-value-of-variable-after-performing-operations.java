7f252ad8f30513686a02d8283758b1ea2474715a
kim-na-ram <snrneh3@naver.com>
2025-10-20 04:11:11 +0000

    Time: 0 ms (100%), Space: 43 MB (34.33%) - LeetHub

--- LeetCode/Easy/2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
+++ LeetCode/Easy/2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
@@ -2,11 +2,8 @@ class Solution {
     public int finalValueAfterOperations(String[] operations) {
         int x = 0;
         for(String operation : operations) {
-            if(operation.contains("+")) {
-                x += 1;
-            } else {
-                x -= 1;
-            }
+            if(operation.contains("+")) x++;
+            else x--;
         }
 
         return x;
