d887cc6ed3da53e8d3176d235be8c852415afa7d
kim-na-ram <snrneh3@naver.com>
2025-10-31 10:28:02 +0000

    Time: 3 ms (36.86%), Space: 46.9 MB (7.71%) - LeetHub

--- LeetCode/Easy/3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.java
+++ LeetCode/Easy/3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.java
@@ -1,17 +1,18 @@
 class Solution {
     public int[] getSneakyNumbers(int[] nums) {
         Map<Integer, Integer> visibility = new HashMap<>();
-        List<Integer> twice = new ArrayList<>();
         for(int num : nums) {
             visibility.put(num, visibility.getOrDefault(num, 0) + 1);
         }
 
+        int[] answer = new int[2];
+        int index = 0;
         for(int key : visibility.keySet()) {
             if(visibility.get(key) == 2) {
-                twice.add(key);
+                answer[index++] = key;
             }
         }
 
-        return twice.stream().mapToInt(Integer::intValue).toArray();
+        return answer;
     }
 }
