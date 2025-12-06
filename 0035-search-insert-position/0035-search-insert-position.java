fe70bfe35ec2bc94a1dd66bd729a4413e409e20d
na.ram <32188154+kim-na-ram@users.noreply.github.com>
2025-12-06 23:23:58 +0900

    Time: 0 ms (100%), Space: 45 MB (9.77%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+
+        while(start <= end) {
+            int mid = (start + end) / 2;
+
+            if(nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                if(nums[mid] == target) { start = mid; break; }
+                start = mid + 1;
+            }
+        }
+
+        return start;
+    }
+}
